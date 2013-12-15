1d3a8dbe6227784884e1523820edbf25667551c2
kirilldavidenko <snoopinf@gmail.com>
2013-12-16 00:13:44 +0200

    Test support

---
+++ SevenWonders/test/com/sevenwoders/TableTest.java
@@ -0,0 +1,14 @@
+package com.sevenwoders;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TableTest {
+	
+	@Test
+	public void testJUnit() {
+		assertEquals(1, 1);
+	}
+
+}
