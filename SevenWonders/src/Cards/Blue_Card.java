ff696ca2bb0aed84451195e5931770896a2a39c4
Jeka-Study <INfliuS_gp@bigmir.net>
2013-12-20 18:08:47 +0400

    Ну я там что-то добавил , наверное криво Т_Т

---
+++ SevenWonders/src/Cards/Blue_Card.java
@@ -0,0 +1,12 @@
+package Cards;
+
+public class Blue_Card extends ABSTRACT_CARD{
+	private int Glory;
+	
+	public int getGlory() {
+		return Glory;
+	}
+	public void setGlory(int glory) {
+		Glory = glory;
+	}
+}
